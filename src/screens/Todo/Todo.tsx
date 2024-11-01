9a565e23b267531773e2dc58bb9a6a1559c18c2c
hassanmahmood-dubizzlelabs <hassan.mahmood@dubizzlelabs.com>
2024-11-01 11:28:50 +0500

    Display todos on screen using Flatlist

--- src/screens/Todo/Todo.tsx
+++ src/screens/Todo/Todo.tsx
@@ -1,14 +1,31 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import { SafeScreen } from '@/components/templates'
 import { useTodo } from '@/store/todo'
-import React from 'react'
+import { useTheme } from '@/theme';
 
 function Todo() {
+  const { gutters, fonts } = useTheme();
   const todos = useTodo((state) => state.todos);
-  console.log(todos);
 
   return (
     <SafeScreen>
+      <ScrollView>
+        <View style={[gutters.paddingHorizontal_12]}>
 
+          <FlatList
+            data={todos}
+            renderItem={
+              ({item}) => <View style={[gutters.paddingTop_12]}>
+                <Text style={[fonts.gray200, fonts.size_16]}>{item.text}</Text>
+              </View>
+            }
+            keyExtractor={item => item.id}
+            />
+
+        </View>
+      </ScrollView>
     </SafeScreen>
   )
 }
