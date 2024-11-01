77aeced206fa916e8aa7b48a962e6c1fa0b5f210
hassanmahmood-dubizzlelabs <hassan.mahmood@dubizzlelabs.com>
2024-11-01 10:32:18 +0500

    Create Todo screen

---
+++ src/screens/Todo/Todo.tsx
@@ -0,0 +1,12 @@
+import { SafeScreen } from '@/components/templates'
+import React from 'react'
+
+function Todo() {
+  return (
+    <SafeScreen>
+
+    </SafeScreen>
+  )
+}
+
+export default Todo
