12ca57277e841fda3857dfb03858d6304b44b6fd
hassanmahmood-dubizzlelabs <hassan.mahmood@dubizzlelabs.com>
2024-11-01 10:12:32 +0500

    Add dummy data to Todo store

--- src/store/todo/index.js
+++ src/store/todo/index.js
@@ -1,5 +1,16 @@
 import { create } from 'zustand';
 
 export const useTodo = create((set) => ({
-  todos: [],
+  todos: [
+    { id: '1', text: 'Buy milk'},
+    { id: '2', text: 'Buy eggs'},
+    { id: '3', text: 'Buy bread'},
+    { id: '4', text: 'Buy butter'},
+    { id: '5', text: 'Buy cheese'},
+    { id: '6', text: 'Buy jam'},
+    { id: '7', text: 'Buy honey'},
+    { id: '8', text: 'Buy tea'},
+    { id: '9', text: 'Buy coffee'},
+    { id: '10', text: 'Buy sugar'},
+  ],
 }));
