4deca034fd9ffc98db2266a25b3f7addd19c2f01
hassanmahmood-dubizzlelabs <hassan.mahmood@dubizzlelabs.com>
2024-11-01 10:46:09 +0500

    Read Todos from todo store

--- src/screens/Todo/Todo.tsx
+++ src/screens/Todo/Todo.tsx
@@ -1,7 +1,11 @@
 import { SafeScreen } from '@/components/templates'
+import { useTodo } from '@/store/todo'
 import React from 'react'
 
 function Todo() {
+  const todos = useTodo((state) => state.todos);
+  console.log(todos);
+
   return (
     <SafeScreen>
 
