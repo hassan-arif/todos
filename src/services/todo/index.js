16a780ef0e53934f7738385be2fb9deffd0bb1b6
hassanmahmood-dubizzlelabs <hassan.mahmood@dubizzlelabs.com>
2024-11-04 15:41:40 +0500

    Fix instance by adding base path

--- src/services/todo/index.js
+++ src/services/todo/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Config from 'react-native-config';
 
 const jsonplaceholder = axios.create({
-  baseURL: Config.API_URL,
+  baseURL: Config.API_URL + "/todos",
 });
 
 export default jsonplaceholder;
