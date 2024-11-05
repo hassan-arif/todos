4916d3965fd0788562b2d8fd37ed0e85c2c7ed63
hassanmahmood-dubizzlelabs <hassan.mahmood@dubizzlelabs.com>
2024-11-05 10:32:51 +0500

    Remove axios instance

--- src/services/todo/index.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import Config from 'react-native-config';
-
-const jsonplaceholder = axios.create({
-  baseURL: Config.API_URL + "/todos",
-});
-
-export default jsonplaceholder;
