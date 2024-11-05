ad0b2fad24ba164e5b5bdf65354d4cb5b977781f
hassanmahmood-dubizzlelabs <hassan.mahmood@dubizzlelabs.com>
2024-11-05 17:08:09 +0500

    Add loading icon (instead of create icon) when processing api request

--- src/screens/Todo/Todo.tsx
+++ src/screens/Todo/Todo.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dimensions, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, Text, TouchableOpacity, View, ActivityIndicator } from 'react-native';
 import { FlatList, TextInput } from 'react-native-gesture-handler';
 import { SafeScreen } from '@/components/templates'
 import { useTheme } from '@/theme';
@@ -18,6 +18,7 @@ function Todo() {
   const deviceWidth = Dimensions.get('window').width;
 
   const todos = useSelector(state => state.todos.todoList);
+  const loading = useSelector(state => state.todos.loading);
 
   const dispatch = useDispatch();
   const [newTodo, setNewTodo] = React.useState('');
@@ -145,19 +146,25 @@ function Todo() {
               placeholder="Add new todo"
               placeholderTextColor={colors.gray200}
               onChangeText={(text) => {setNewTodo(text)}}
+              autoFocus = {true}
+              editable = {!loading}
             />
-            <TouchableOpacity onPress={() => {
-              dispatch(createTodos({
-                userId: 1, 
-                title: newTodo, 
-                completed: true
-              }));
-              setNewTodo('');
-            }} style={[
-              layout.itemsCenter
-            ]}>
-              <IconByVariant path={'add'} stroke={colors.gray400} />
-            </TouchableOpacity>
+            {loading ? (
+              <ActivityIndicator size="small" color={colors.gray400} />
+            ) : (
+              <TouchableOpacity onPress={() => {
+                dispatch(createTodos({
+                  userId: 1, 
+                  title: newTodo, 
+                  completed: true
+                }));
+                setNewTodo('');
+              }} style={[
+                layout.itemsCenter
+              ]}>
+                <IconByVariant path={'add'} stroke={colors.gray400} />
+              </TouchableOpacity>
+            )}
           </View>
         </View>
       </View>
