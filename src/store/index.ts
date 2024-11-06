a442f06879516f8bfb11862ce9fc744ced2611ee
hassanmahmood-dubizzlelabs <hassan.mahmood@dubizzlelabs.com>
2024-11-06 11:09:39 +0500

    Configure store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from './todoSlice';
+import { todoApi } from "./todoApi";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
     todos: todoReducer,
+    [todoApi.reducerPath]: todoApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todoApi.middleware),
 });
+setupListeners(store.dispatch);
