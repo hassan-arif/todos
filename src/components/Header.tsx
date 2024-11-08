8380e4c01c25807a25ccebf84f8dd1a4b558daae
hassanmahmood-dubizzlelabs <hassan.mahmood@dubizzlelabs.com>
2024-11-08 15:53:40 +0500

    Improve Header's conditional rendering

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,22 @@
 import { useTheme } from "@/theme";
 import { Text } from "react-native";
 
-export default function Header(props: { count: string }) {
+export default function Header(props: { count: number }) {
   /**
    * Header component is a simple component that displays the title of the app and the count of tasks.
-   * It receives string of tasks' count or an empty one.
+   * It receives tasks' count.
    */
-  
+
   const { gutters, fonts
   } = useTheme();
+
+  if (!props.count) {
+    return (
+      <Text style={[
+        fonts.size_32, fonts.gray200, fonts.bold, gutters.marginTop_16, gutters.marginBottom_16
+      ]}>Task List</Text>
+    )
+  }
 
   return (
     <Text style={[
