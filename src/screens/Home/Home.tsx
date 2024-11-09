8bb75570d5b503350c18ca78f22ba2f1a8752655
hassanmahmood-dubizzlelabs <hassan.mahmood@dubizzlelabs.com>
2024-11-09 07:28:54 +0500

    Refactor Home Screen

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Dimensions, Text, TouchableOpacity, View, ActivityIndicator, Button, StyleSheet } from 'react-native';
-import { FlatList, TextInput } from 'react-native-gesture-handler';
+import { Dimensions, Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import { SafeScreen } from '@/components/templates'
 import { useTheme } from '@/theme';
 import { IconByVariant } from '@/components/atoms';
@@ -13,7 +13,7 @@ import CustomButton from '@/components/CustomButton';
 const deviceWidth = Dimensions.get('window').width;
 
 export default function Home() {
-  const { layout, gutters, fonts, colors, borders
+  const { layout, gutters, fonts, colors
   } = useTheme();
   
   let { data, error, isLoading, refetch } = useGetTodosQuery();
@@ -139,7 +139,7 @@ export default function Home() {
         ]}
       >
         <View>
-          <Text style={[fonts.size_32, fonts.gray200, fonts.bold, gutters.marginTop_16, gutters.marginBottom_16]}>Todo List {data ? `(${data.length})` : ''}</Text>
+          <Header count={data?.length} />
         
           <FlatList
             style={{marginBottom: 180}}
@@ -148,50 +148,9 @@ export default function Home() {
             keyExtractor={item => item.id}
           />
         </View>
-        {isModalVisible ? (
-          <CustomModal 
-            isCreate={true}
-            isVisible={isModalVisible} 
-            toggleVisibility={toggleModal}
-
-            headerText='Add New Item'
-            defaultTodo=''
-
-            addTodo={addNewTodo}
-          />
-
-          //   <View style={{ margin: 20, backgroundColor:'white'}}>
-          //     <Text>Hello!</Text>
-          //     <TextInput
-          //       style={[
-          //         fonts.gray200, 
-          //         fonts.size_16, 
-          //       ]}
-          //       defaultValue={newTodo}
-          //       placeholder="Add new todo"
-          //       placeholderTextColor={colors.gray200}
-          //       onChangeText={(text) => {setNewTodo(text)}}
-          //       autoFocus = {true}
-          //     />
-
-          //     {isLoading ? (
-          //       <ActivityIndicator size="small" color={colors.gray400} />
-          //     ) : (
-          //       <TouchableOpacity onPress={() => {
-          //         addNewTodo()
-          //         toggleModal()
-          //       }} style={[
-          //         layout.itemsCenter
-          //       ]}>
-          //         <IconByVariant path={'create'} width={64} height={64} />
-          //       </TouchableOpacity>
-          //     )}
-    
-          //     <Button title="Hide modal" onPress={toggleModal} />
-          //   </View>
-          // </Modal>
-        ): null}
-        {!isModalVisible ? 
+        
+        {
+          !isModalVisible && 
           <View style={styles.createButton}>
             <CustomButton
               isLoading={isLoading} 
@@ -200,7 +159,18 @@ export default function Home() {
               size={64}
             />
           </View>
-        : null}
+        }
+
+        <CustomModal 
+          isCreate={true}
+          isVisible={isModalVisible} 
+          toggleVisibility={toggleModal}
+
+          headerText='Add New Item'
+          defaultTodo=''
+
+          addTodo={addNewTodo}
+        />
       </View>
     </SafeScreen>
   )
